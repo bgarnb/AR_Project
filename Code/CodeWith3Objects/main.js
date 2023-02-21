0a450f6ae114316e234caed1c13ba1f3a1366fe7
bgarnb <bgarnelisch@gmail.com>
2023-02-21 14:36:34 +0200

    Initialize repo

---
+++ Code/CodeWith3Objects/main.js
@@ -0,0 +1,104 @@
+import {loadGLTF, loadAudio, loadVideo} from "../../libs/loader.js";
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    //initiate the AR 3 object
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: '../../assets/targets/targets.mind'
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+//light is needed when we use 3D objects
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
+
+    const raccoon = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
+    raccoon.scene.position.set(0, -0.4, 0);
+
+    const bear = await loadGLTF('../../assets/models/musicband-bear/scene.gltf');
+    bear.scene.scale.set(0.1, 0.1, 0.1);
+    bear.scene.position.set(0, -0.4, 0);
+
+    const video = await loadVideo("../../assets/videos/sintel/sintel.mp4");
+    const texture = new THREE.VideoTexture(video);
+    const geometry = new THREE.PlaneGeometry(1, 204/480);
+    const material = new THREE.MeshBasicMaterial({map: texture});
+    const plane = new THREE.Mesh(geometry, material);
+
+    //first digital content (3D model with audio)
+
+    const raccoonAnchor = mindarThree.addAnchor(0);
+    raccoonAnchor.group.add(raccoon.scene);
+
+    const audioClip1 = await loadAudio('../../assets/sounds/musicband-background.mp3');
+
+    const listener1 = new THREE.AudioListener();
+    camera.add(listener1);
+
+    const audio1 = new THREE.PositionalAudio(listener1);
+    raccoonAnchor.group.add(audio1);
+
+    audio1.setBuffer(audioClip1);
+    audio1.setRefDistance(100);
+    audio1.setLoop(true);
+
+    raccoonAnchor.onTargetFound = () => {
+      audio1.play();
+    }
+    raccoonAnchor.onTargetLost = () => {
+      audio1.pause();
+    }
+
+    //second digital content (3D model with audio)
+
+    const bearAnchor = mindarThree.addAnchor(1);
+    bearAnchor.group.add(bear.scene);
+
+    const audioClip2 = await loadAudio('../../assets/sounds/musicband-background.mp3');
+
+    const listener2 = new THREE.AudioListener();
+    camera.add(listener2);
+
+    const audio2 = new THREE.PositionalAudio(listener2);
+    bearAnchor.group.add(audio2);
+
+    audio2.setBuffer(audioClip2);
+    audio2.setRefDistance(100);
+    audio2.setLoop(true);
+
+    bearAnchor.onTargetFound = () => {
+      audio2.play();
+    }
+    bearAnchor.onTargetLost = () => {
+      audio2.pause();
+    }
+
+    // third digital content (video)
+    const anchor = mindarThree.addAnchor(2);
+    anchor.group.add(plane);
+
+    anchor.onTargetFound = () => {
+      video.play();
+    }
+    anchor.onTargetLost = () => {
+      video.pause();
+    }
+    video.addEventListener( 'play', () => {
+      video.currentTime = 6;
+    });
+//start the experience
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  //to πλήκτρο start δουλεύει για μια φορά μόνο
+  //const startButton = document.createElement("button");
+  //startButton.textContent = "Start";
+  //startButton.addEventListener("click", start);
+  //document.body.appendChild(startButton);
+  start();
+});
---
+++ README.md
@@ -0,0 +1,61 @@
+=== Summary
+
+1. ./lectures-start directory contains source code at the beginning of the lectures
+2. ./lectures directory contains source at the end of the lectures
+3. ./assets directory contains all resources used in the examples
+4. ./libs contains all libraries used in the examples
+
+=== Lectures and Source Code Mappings:
+
+-- Section 3 --
+
+3D Rendering Basic: 110-threejs 
+
+-- Section 4 --
+
+Quick Start: 200-image-start 
+Mock WebCam: 201-camera-mock
+Custom Container: 202-custom-container
+Import 3D Models: 210-models
+Multiple Image Targets: 211-multi-detect
+Multiple Targets Tracking: 212-multi-track
+Animations: 213-animations
+Events Handling: 220-events
+Audio Contents: 221-audio
+User Interactions: 222-user-interactions
+Video Contents: 230-video
+Chroma Videos: 231-chroma-video
+Fix Autostart: 232-fix-autostart
+CSS Renderer: 240-css-renderer
+Streaming Vimeo: 241-vimeo
+Streaming Youtube: 242-youtube
+Custom UI/UX: custom-ui
+Assignment: 360-image-tracking-assignment
+
+-- Section 5 --
+Quick Start: 300-face-start
+Import 3D Models: 310-model-accessory
+Occluders: 311-occluder
+FaceMesh: 320-facemesh
+Switch Cameras: 330-switch-camera
+Photo Capure: 331-photo-capture
+Web Share: 332-web-share
+Assignment: 340-face-tracking-assignment
+
+-- Section 6 --
+WebXR Quick Start: 400-webxr-start
+ARButton: 401-ar-button
+Controllers: 410-controller
+Place Objects: 411-place-object
+HitTests:: 420-hit-test
+Assignment: 430-furniture
+
+-- Section 7 --
+Sharing Camera Feed: 500-video-feed
+Hand Gestures Detection: 501-hand-gesture
+Face Emotions Detection: 502-face-api
+
+-- Section 8 -- 
+Introduction to AFRAME: 600-aframe
+AFRAME and WebXR: 601-aframe-webxr
+AFRAME and MindAR: 602-aframe-mindar
